b75c79ec08a4a18a3e1fd9a8f69e3292b4e0520c
zahra-alshami <zahrash.95@outlook.com>
2022-12-20 01:15:45 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace svu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
