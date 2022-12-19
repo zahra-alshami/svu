822f815ba3f73507ef895f81b8bc21166fbc604d
zahra-alshami <zahrash.95@outlook.com>
2022-12-20 01:54:02 +0200

    product and product type with relation

---
+++ Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace svu
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { set; get; }
+        public double ProductPrice { get; set; }
+        public int TypeId { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace svu
 {
@@ -6,6 +7,25 @@ namespace svu
     {
         static void Main(string[] args)
         {
+            Product_Type pt1 = new Product_Type();
+            pt1.TypeName = "type1";
+            pt1.TypeId = 1;
+            Product_Type pt2 = new Product_Type();
+            pt2.TypeName = "type2";
+            pt2.TypeId = 1;
+            Product_Type pt3 = new Product_Type();
+            pt3.TypeName = "type3";
+            pt3.TypeId = 1;
+            List<Product_Type> types = new List<Product_Type>();
+            types.Add(pt1);
+            types.Add(pt2);
+            types.Add(pt3);
+            Product p = new Product();
+            p.ProductId = 1;
+            p.ProductName = "p1";
+            p.ProductPrice = 300;
+            p.TypeId = pt1.TypeId;
+            Console.WriteLine(types.Find(x => x.TypeId == p.TypeId).TypeName);
         }
     }
 }
